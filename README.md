9343dce962f7b4da6e2b76fec8735674965f667b
kks-bakaneko <92.kangsan.kim@gmail.com>
2023-08-25 18:19:37 +0900

    change to xcode

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # a7cutil_for_mac
 Sony A7C Camera Remote tool for mac
+Start with XCode for Project operation
---
+++ a7cutil_for_mac/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  a7cutil_for_mac
+//
+//  Created by kks on 8/25/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
