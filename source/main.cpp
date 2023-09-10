d9e836e691f8149da595f48b4a333d4555ad1225
kks-bakaneko <92.kangsan.kim@gmail.com>
2023-09-10 18:02:28 +0900

    test 2

---
+++ source/main.cpp
@@ -0,0 +1,21 @@
+#include <chrono>
+#include <filesystem>
+namespace fs = std::filesystem;
+#include <fstream>
+#include <thread>
+#include "CRSDK/CrDeviceProperty.h"
+#include <iostream>
+#include "CRSDK/CameraRemote_SDK.h"
+#include <termios.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <vector>
+#include <dirent.h>
+
+namespace SDK = SCRSDK;
+using namespace std;
+int main() {
+    cout << "Hello World." << endl;
+    return 0;
+}
